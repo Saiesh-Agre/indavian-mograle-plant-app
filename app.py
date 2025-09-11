cf2278d0ed0c2d94fb21bcf122ee4e44f69fb3da
Saiesh-Agre <saiesh.agre777@gmail.com>
2025-09-11 18:30:04 +0530

    Updated pie chart legend location

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ with row1_col2:
         ax.legend(
             wedges,
             legend_labels,
-            loc="center left",
+            loc="center bottom",
             bbox_to_anchor=(1, 0, 0.5, 1),
             labelcolor='white',
             frameon=False
