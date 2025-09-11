00e7b5a6b38f29566b691448395935e4e20e012f
Saiesh-Agre <saiesh.agre777@gmail.com>
2025-09-11 18:20:12 +0530

    Saved video height change

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ with row2_col1:
                 '''
 
         video_html += '</div>'
-        components.html(video_html, height=600)
+        components.html(video_html, height=650)
     else:
         st.info("No clips available.")
 
